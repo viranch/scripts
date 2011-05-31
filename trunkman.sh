7bb91e725d5b62f27bd8e4d1d1219b20062f8dbc
Viranch Mehta <viranch.mehta@gmail.com>
2011-05-31 16:05:14 +0530

    Implement uninstall in trunkman

--- trunkman.sh
+++ trunkman.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
-if [ $# -eq 0 ]; then
-	echo "Specify a package file."
+function _remove() {
+	for i in $(tar tf $1)
+	do
+		rm $(echo $i | sed 's/^usr\//\/home\/viranch\/kde\/install\//g')
+	done
+}
+
+
+function _install() {
+	mkdir -p /tmp/trunkman.tmp
+	cd /tmp/trunkman.tmp
+	tar -xvf $1 --exclude .PKGINFO --exclude .INSTALL --overwrite
+	cp -R usr/* ~/kde/install/
+	cd /tmp
+	rm -rf /tmp/trunkman.tmp
+}
+
+if [ $# -lt 2 ]; then
+	echo "Usage: $0 install|remove path_to_pkg"
 	exit 0
 fi
-cd ~/kde
-tar -xvf $1 --exclude .PKGINFO --exclude .INSTALL --overwrite
-cp -R usr/* install/
-rm -rf usr
+
+_$1 $2
 
