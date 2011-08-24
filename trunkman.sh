5ebec5e8ee8cc42708fe643940bd44a367f0f076
Viranch Mehta <viranch.mehta@gmail.com>
2011-08-24 17:03:17 +0530

    Make trunkman remove the directories created by the package

--- trunkman.sh
+++ trunkman.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 function _remove() {
-	for i in $(tar tf $1)
+	for i in $(pacman -Qql $1|grep /$ -v)
 	do
-		rm $(echo $i | sed 's/^usr\//\/home\/viranch\/kde\/install\//g')
+		rm $(echo $i | sed 's/^\/usr\//\/home\/viranch\/kde\/install\//g')
 	done
+
+    for i in $(pacman -Qql $1|grep $1/$)
+    do
+        rm -rf $i
+    done
 }
 
 
 function _install() {
+    pkg_file=$(ls /var/cache/pacman/pkg/$1*|tail -n1)
 	mkdir -p /tmp/trunkman.tmp
 	cd /tmp/trunkman.tmp
 	tar -xvf $1 --exclude .PKGINFO --exclude .INSTALL --overwrite
@@ -17,7 +23,7 @@ function _install() {
 }
 
 if [ $# -lt 2 ]; then
-	echo "Usage: $0 install|remove path_to_pkg"
+	echo "Usage: $0 install|remove pkg"
 	exit 0
 fi
 
