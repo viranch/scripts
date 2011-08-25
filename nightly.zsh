3e676e4a4938ea23fe268dd46a32b6234a96df9f
Viranch Mehta <viranch.mehta@gmail.com>
2011-08-25 17:00:00 +0530

    Update scripts

--- nightly.zsh
+++ nightly.zsh
@@ -1,8 +1,12 @@
 #!/bin/zsh
 
+# Script to do time-consuming stuff that does not need your attention
+# while you go to sleep, and suspend the computer after the job is done
+
 rm -f /tmp/shutdown.file
 su -c "(while [ ! -f \"/tmp/shutdown.file\" ]; do sleep 60; done; pm-suspend) &"
 
-zsh ~/kde/doit.sh
+## Do time consuming stuff here (compile linux?) while you sleep
+
 touch /tmp/shutdown.file
 
