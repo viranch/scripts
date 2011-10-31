40f3b8b2ff87b2ac5753c1070d6ad7214882fda4
Viranch Mehta <viranch.mehta@gmail.com>
2011-11-01 05:07:50 +0530

    Dont force the user to enter the current interface in use, just replace all of them with new

--- change_iface.sh
+++ change_iface.sh
@@ -1,2 +1,2 @@
-sed -i "s/$2/$3/g" $1/conky-arch.conf
-sed -i "s/$2/$3/g" $1/clock_rings.lua
+sed -e "s/eth0/$2/g" -e "s/wlan0/$2/g" -e "s/ppp0/$2/g" -e "s/usb0/$2/g" -i $1/conky-arch.conf
+sed -e "s/eth0/$2/g" -e "s/wlan0/$2/g" -e "s/ppp0/$2/g" -e "s/usb0/$2/g" -i $1/clock_rings.lua
