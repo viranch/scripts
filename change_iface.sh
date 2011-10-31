ca4dab1f2fdfe5cfe5de42fcb28a2d69e53eae25
Viranch Mehta <viranch.mehta@gmail.com>
2011-11-01 05:10:11 +0530

    Remove unnecessary zeros

--- change_iface.sh
+++ change_iface.sh
@@ -1,2 +1,2 @@
-sed -e "s/eth0/$2/g" -e "s/wlan0/$2/g" -e "s/ppp0/$2/g" -e "s/usb0/$2/g" -i $1/conky-arch.conf
-sed -e "s/eth0/$2/g" -e "s/wlan0/$2/g" -e "s/ppp0/$2/g" -e "s/usb0/$2/g" -i $1/clock_rings.lua
+sed -e "s/eth/$2/g" -e "s/wlan/$2/g" -e "s/ppp/$2/g" -e "s/usb/$2/g" -i $1/conky-arch.conf
+sed -e "s/eth/$2/g" -e "s/wlan/$2/g" -e "s/ppp/$2/g" -e "s/usb/$2/g" -i $1/clock_rings.lua
