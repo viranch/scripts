6341220127517d04618d16f62b0994677bf1f1ee
Viranch Mehta <viranch.mehta@gmail.com>
2011-11-15 16:48:02 +0530

    Run only one instance of the shuffler

--- shufflebg.sh
+++ shufflebg.sh
@@ -2,11 +2,22 @@ bin="feh"
 args="--bg-scale"
 interval=60
 
+if [ -f "/tmp/wp.pid" ]; then
+    kill -0 `cat /tmp/wp.pid`
+    ret=$?
+    if [ $ret -eq 0 ]; then
+        echo "Another instance of the shuffler is already running. Exiting..."
+        exit 2
+    fi
+fi
+
 wpdir="$1"
 if [ ! -d "$wpdir" ]; then
     echo ""$wpdir:" does not exist"
     exit 1
 fi
+
+echo $$ > /tmp/wp.pid
 find "$wpdir" -name "*.jpg" -type f > /tmp/wplist
 count=$(wc -l < /tmp/wplist)
 
