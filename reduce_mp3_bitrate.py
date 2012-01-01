60ea9cfdcc253323abc798fbc696137a0179c557
Viranch Mehta <viranch.mehta@gmail.com>
2012-01-01 14:42:12 +0530

    Fix bug: When input path ends with /, basename returns empty string causing havoc

--- reduce_mp3_bitrate.py
+++ reduce_mp3_bitrate.py
@@ -13,25 +13,25 @@ BIT_RATE = 128
 
 in_dir = sys.argv[1]
 out_dir = sys.argv[2]
+if in_dir.endswith('/'): in_dir = in_dir[:-1]
+root_path = in_dir.replace('/'+os.path.basename(in_dir), '')
 
 # Returns '/tmp/Music/English/Akon' from '/home/viranch/data/Music/English/Akon'
-def get_dest (srcpath):
-    path = in_dir.replace('/'+os.path.basename(in_dir), '')
-    print srcpath, path
-    return srcpath.replace(path, out_dir+'/')
+def get_dest(srcpath):
+    return srcpath.replace(root_path, out_dir+'/')
 
 def convert(path):
     dest=get_dest(path)
     try: os.makedirs(dest)
     except: pass
-    for i in os.listdir (path):
+    for i in os.listdir(path):
         src=path+'/'+i
         if '.mp3' in i:
             target=dest+'/'+i
             print src, '==>', target
-            subprocess.call (['lame', '-F', '-b', str(BIT_RATE), src, target])
+            subprocess.call(['lame', '-F', '-b', str(BIT_RATE), src, target])
             print ''
         elif os.path.isdir(src):
-            convert (src)
+            convert(src)
 
 convert(in_dir)
