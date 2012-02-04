ab0dac863c5941389e9beadbbaf4f5afd5f537d0
Viranch Mehta <viranch.mehta@gmail.com>
2012-02-04 18:10:17 +0530

    Show unread emails in folders other than Inbox too

--- webmail.py
+++ webmail.py
@@ -20,50 +20,68 @@ opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
 opener.addheaders = [('Host','webmail.daiict.ac.in'),
                      ('User-Agent','Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.77 Safari/535.7')]
 
-test_string  = '<b>Mobile</b> is recommended for mobile devices. <br><br>To set <b>Default</b> to be your preferred client type, change the login options in your Preferences, General tab after you log in.</div>'
-while True:
-    password = getpass.getpass()
-    f = opener.open('http://webmail.daiict.ac.in/zimbra/?loginOp=login&username='+username+'&password='+password+'&client=standard')
-    s = f.read()
-    f.close()
-    if test_string not in s: break
-    print 'Sorry, try again.'
+def login():
+    test_string  = '<b>Mobile</b> is recommended for mobile devices. <br><br>To set <b>Default</b> to be your preferred client type, change the login options in your Preferences, General tab after you log in.</div>'
+    while True:
+        password = getpass.getpass()
+        f = opener.open('http://webmail.daiict.ac.in/zimbra/?loginOp=login&username='+username+'&password='+password+'&client=standard')
+        s = f.read()
+        f.close()
+        if test_string not in s: break
+        print 'Sorry, try again.'
+    return s
 
 def parse(s, start, end):
     s = s[s.find(start)+len(start):]
     return HTMLParser.HTMLParser().unescape(s[:s.find(end)])
 
+def check_emails(s):
+    folders = []
+    while True:
+        st = s.find('<td nowrap colspan')
+        if st<0: break
+        s = s[st+1:]
+        link = 'http://webmail.daiict.ac.in'+parse(s, '<a href=\'', '\'')
+        s = s[s.find('<span title=')+1:]
+        folder = s[s.find('>')+1:s.find('<')]
+        if '(' in folder:
+            tok = folder.split('\n')
+            name = tok[0]
+            if name in ['Drafts', 'Spam', 'Trash']: continue
+            count = tok[1]
+            count = count[count.find('(')+1:count.find(')')]
+            folders.append([link, name, count])
+    return folders
+
+def show_emails(s):
+    emails = []
+    while True:
+        st = s.find('<tr onclick')
+        if st<0: break
+        s = s[st+1:]
+        cl = parse(s, 'class=\'', '\'')
+        if 'Unread' not in cl: continue
+        sender = parse(s, '<td>', '\n')
+        subj = parse(s, '<span class="Fragment">', '</span>')
+        frag = parse(s, '<span class=\'Fragment\'>', '</span>')
+        when = parse(s, '<td nowrap align="right">\n\t\t\t\t\t\t\t\t\t', '\n')
+        emails.append([sender, subj, when, frag])
+
+    for email in emails:
+        print 'From:', email[0]
+        print 'Subject:', email[1]
+        print 'Received:', email[2]
+        print email[3]
+        print ''
+
+s = login()
 print ''
-while True:
-    st = s.find('<span title=')
-    if st<0: break
-    s = s[st+1:]
-    folder = s[s.find('>')+1:s.find('<')]
-    if '(' in folder:
-        tok = folder.split('\n')
-        name = tok[0]
-        count = tok[1]
-        count = count[count.find('(')+1:count.find(')')]
-        print count, 'unread email(s) in', name
-
-emails = []
-while True:
-    st = s.find('<tr onclick')
-    if st<0: break
-    s = s[st+1:]
-    cl = parse(s, 'class=\'', '\'')
-    if 'Unread' not in cl: continue
-    sender = parse(s, '<td>', '\n')
-    subj = parse(s, '<span class="Fragment">', '</span>')
-    frag = parse(s, '<span class=\'Fragment\'>', '</span>')
-    when = parse(s, '<td nowrap align="right">\n\t\t\t\t\t\t\t\t\t', '\n')
-    emails.append([sender, subj, when, frag])
-
-if len(emails)>0: print '\n', 'Inbox:'
-for email in emails:
-    print 'From:', email[0]
-    print 'Subject:', email[1]
-    print 'Received:', email[2]
-    print email[3]
-    print ''
-
+for folder in check_emails(s):
+    if folder[1] == "Inbox":
+        sf = s
+    else:
+        f = opener.open(folder[0])
+        sf = f.read()
+        f.close()
+    print folder[2], 'unread email(s) in', folder[1]
+    show_emails(sf)
