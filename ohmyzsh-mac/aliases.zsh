bf5c92aefb0222811a61eb9f36300d74161d04c5
Viranch Mehta <viranch.mehta@gmail.com>
2012-05-07 14:23:27 +0530

    Add a set of alternate config files for MacOS

---
+++ ohmyzsh-mac/aliases.zsh
@@ -0,0 +1,19 @@
+# Super user
+alias s='sudo'
+
+# Show history
+alias history='fc -l 1'
+
+# List direcory contents
+alias ll='ls -lah'
+alias l='ls -lh'
+
+alias afind='ack-grep -il'
+
+alias x=extract
+
+# Command aliases
+alias wget='wget --read-timeout=10'
+alias swget='swget --read-timeout=10'
+alias utube='youtube-dl -ct'
+alias v='vim'
---
+++ ohmyzsh-mac/git.zsh
@@ -0,0 +1,73 @@
+# get the name of the branch we are on
+function git_prompt_info() {
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || return
+  echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
+}
+
+parse_git_dirty () {
+  if [[ -n $(git status -s 2> /dev/null) ]]; then
+    echo "$ZSH_THEME_GIT_PROMPT_DIRTY"
+  else
+    echo "$ZSH_THEME_GIT_PROMPT_CLEAN"
+  fi
+}
+
+# get the status of the working tree
+git_prompt_status() {
+  INDEX=$(git status --porcelain 2> /dev/null)
+  STATUS=""
+  if $(echo "$INDEX" | grep '^?? ' &> /dev/null); then
+    STATUS="$ZSH_THEME_GIT_PROMPT_UNTRACKED$STATUS"
+  fi
+  if $(echo "$INDEX" | grep '^A  ' &> /dev/null); then
+    STATUS="$ZSH_THEME_GIT_PROMPT_ADDED$STATUS"
+  elif $(echo "$INDEX" | grep '^M  ' &> /dev/null); then
+    STATUS="$ZSH_THEME_GIT_PROMPT_ADDED$STATUS"
+  fi
+  if $(echo "$INDEX" | grep '^ M ' &> /dev/null); then
+    STATUS="$ZSH_THEME_GIT_PROMPT_MODIFIED$STATUS"
+  elif $(echo "$INDEX" | grep '^AM ' &> /dev/null); then
+    STATUS="$ZSH_THEME_GIT_PROMPT_MODIFIED$STATUS"
+  elif $(echo "$INDEX" | grep '^ T ' &> /dev/null); then
+    STATUS="$ZSH_THEME_GIT_PROMPT_MODIFIED$STATUS"
+  fi
+  if $(echo "$INDEX" | grep '^R  ' &> /dev/null); then
+    STATUS="$ZSH_THEME_GIT_PROMPT_RENAMED$STATUS"
+  fi
+  if $(echo "$INDEX" | grep '^ D ' &> /dev/null); then
+    STATUS="$ZSH_THEME_GIT_PROMPT_DELETED$STATUS"
+  fi
+  if $(echo "$INDEX" | grep '^UU ' &> /dev/null); then
+    STATUS="$ZSH_THEME_GIT_PROMPT_UNMERGED$STATUS"
+  fi
+  echo $STATUS
+}
+
+# Git aliases
+function current_branch() {
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || return
+  echo ${ref#refs/heads/}
+}
+
+alias g='git'
+compdef _git g=git
+alias gi='git init'
+compdef _git gi=git-init
+alias gcl='git clone'
+compdef _git gcl=git-clone
+alias gco='git checkout'
+compdef _git gco=git-checkout
+alias gst='git status'
+compdef _git gst=git-status
+alias gl='git pull'
+compdef _git gll=git-pull
+alias gd='git diff'
+compdef _git gd=git-diff
+alias gcm='git commit'
+compdef _git gcm=git-commit
+alias gc='git commit -am'
+compdef _git gc=git-commit
+alias gp='git push'
+compdef _git gp=git-push
+alias glg='git log --max-count=5'
+compdef _git glg=git-log
