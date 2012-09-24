fb85371764099a663832478125975a5f226c2be2
Viranch Mehta <viranch.mehta@gmail.com>
2012-09-24 10:43:30 +0530

    Add alias for sudoedit

--- ohmyzsh-common/aliases.zsh
+++ ohmyzsh-common/aliases.zsh
@@ -22,6 +22,7 @@ alias cont='kill -CONT'
 alias term='kill -TERM'
 alias kll='kill -KILL'
 alias aws='ssh -i ~/.ssh/viranch.pem arch@ec2-184-169-223-159.us-west-1.compute.amazonaws.com'
+alias sv='SUDO_EDITOR=vim sudoedit' #sudo vim
 
 # Dev aliases
 alias h='vim `echo $_|sed "s/\.cpp/.h/g"`'
