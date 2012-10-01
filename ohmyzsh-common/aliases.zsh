0e3b1870e3a3fddc21efdd854e6800f707a73efb
Viranch Mehta <viranch.mehta@gmail.com>
2012-10-01 11:04:14 +0530

    Add an alias for very commonly used 'ps aux|grep foo'

--- ohmyzsh-common/aliases.zsh
+++ ohmyzsh-common/aliases.zsh
@@ -24,6 +24,7 @@ alias term='kill -TERM'
 alias kll='kill -KILL'
 alias aws='ssh -i ~/.ssh/viranch.pem arch@ec2-184-169-223-159.us-west-1.compute.amazonaws.com'
 alias sv='SUDO_EDITOR=vim sudoedit' #sudo vim
+alias pg='ps aux | grep'
 
 # Dev aliases
 alias h='vim `echo $_|sed "s/\.cpp/.h/g"`'
