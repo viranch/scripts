b89c0381f6569914a74b088053dfd5d4da27b04a
Viranch Mehta <viranch.mehta@gmail.com>
2012-10-22 11:33:37 +0530

    Add aws server name as variable

--- ohmyzsh-common/aliases.zsh
+++ ohmyzsh-common/aliases.zsh
@@ -27,6 +27,7 @@ alias kll='kill -KILL'
 alias sv='SUDO_EDITOR=vim sudoedit' #sudo vim
 alias pg='ps aux | grep'
 alias ss='ssh'
+aws='ec2-184-169-223-159.us-west-1.compute.amazonaws.com'
 
 # Dev aliases
 alias h='vim `echo $_|sed "s/\.cpp/.h/g"`'
