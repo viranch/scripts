8ca2224732b942bc1ced86620ae94eb611bfc80e
Viranch Mehta <viranch.mehta@gmail.com>
2012-10-24 13:13:35 +0530

    Add python script for xdg-open on mac using PyQt and QDesktopServices

---
+++ qt-open.py
@@ -0,0 +1,11 @@
+# script to open specified file with associated application
+# Usage: qt-open ~/some/file.avi
+
+import sys
+
+try:
+    from PyQt4.QtCore import QUrl
+    from PyQt4.QtGui import QDesktopServices
+    QDesktopServices.openUrl(QUrl.fromLocalFile(sys.argv[1]))
+except:
+    pass
