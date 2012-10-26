adb0764350aca6c041baff4d219d0af0e1381b01
Viranch Mehta <viranch.mehta@gmail.com>
2012-10-26 14:59:33 +0530

    Open current directory in file manager by default

--- qt-open.py
+++ qt-open.py
@@ -7,5 +7,7 @@ try:
     from PyQt4.QtCore import QUrl
     from PyQt4.QtGui import QDesktopServices
     QDesktopServices.openUrl(QUrl.fromLocalFile(sys.argv[1]))
+except IndexError:
+    QDesktopServices.openUrl(QUrl.fromLocalFile('.'))
 except:
     pass
