1fc053984842ac1ca9bac36da3f8647c888c1c81
Viranch Mehta <viranch.mehta@gmail.com>
2013-05-14 23:08:01 +0530

    qt-open.py- Append to argv array instead of exception-handling in case of no args

--- qt-open.py
+++ qt-open.py
@@ -3,11 +3,11 @@
 
 import sys
 
+sys.argv.append('.')
+
 try:
     from PyQt4.QtCore import QUrl
     from PyQt4.QtGui import QDesktopServices
     QDesktopServices.openUrl(QUrl.fromLocalFile(sys.argv[1]))
-except IndexError:
-    QDesktopServices.openUrl(QUrl.fromLocalFile('.'))
 except:
     pass
