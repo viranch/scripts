de832a552f9c692f4fbd83393b0b5fb7b2a291b2
Viranch Mehta <viranch.mehta@gmail.com>
2013-05-17 03:20:42 +0530

    tv- implement pretty options and verbosity

--- online/tv.sh
+++ online/tv.sh
@@ -10,13 +10,36 @@
 # Transmission/uTorrent can watch for .torrent files in the download directory.
 #
 
-link="$1"
-dirpath="$2"
+usage() {
+    cat << EOF
+Usage: $0 -l <link-to-rss-feed> -o <output-directory> [-s <search-suffix>]
+EOF
+}
+
+link=""
+dirpath=""
+suff=""
+while getopts "l:o:s:h" OPTION; do
+    case $OPTION in
+        l)
+            link="$OPTARG"
+            ;;
+        o)
+            dirpath="$OPTARG"
+            ;;
+        s)
+            suff=" $OPTARG"
+            ;;
+        h)
+            usage
+            ;;
+    esac
+done
 
 # validate input link
 match=$(echo $link | grep -o "^http://www\.mytvrss\.com/tvrss\.xml?id=[0-9]\+$")
 test -z "$match" && echo "Invalid URL. Please visit mytvrss.com to generate your personalised URL" && exit 1
-test -d "$dirpath" || echo "Invalid download path: $dirpath" && exit 2
+test -d "$dirpath" || (echo "Invalid download path: $dirpath" && exit 2)
 
 # determine 'yesterday'
 platform=`uname`
@@ -30,9 +53,18 @@ else
 fi
 
 # download .torrent for shows aired yesterday
+echo "Getting episode list..."
 curl -s $link | grep $yest -B5 | grep  ">.* S[0-9]\+E[0-9]\+" -o | sed 's/>//g' | while read title
 do
-   echo "Downloading '$title'..."
-   hash=$(curl -s http://torrentz.in/feed?q=`echo $title | sed 's/ /+/g'` | grep "<link>.*$" -o | head -n2 | tail -n1 | sed 's/<link>http:\/\/torrentz\.in\///g' | sed 's/<\/link>//g' | tr '[a-z]' '[A-Z]')
-   curl -s --compressed http://torcache.net/torrent/$hash.torrent > "$dirpath/`echo $title | sed 's/ /./g'`.torrent"
+    title="$title$suff"
+    query=`echo "$title" | sed 's/ /+/g'`
+    echo -n "Searching '$title'... "
+    hash=$(curl -s http://torrentz.in/feed?q=$query | grep "<link>.*$" -o | head -n2 | grep -v "search?q=" | sed 's/<link>http:\/\/torrentz\.in\///g' | sed 's/<\/link>//g')
+    if [[ -n $hash ]]; then
+        echo "found"
+        fname="`echo $title | sed 's/ /./g'`.torrent"
+        curl -s --compressed http://torcache.net/torrent/$hash.torrent > "$dirpath/$fname"
+    else
+        echo "failed"
+    fi
 done
