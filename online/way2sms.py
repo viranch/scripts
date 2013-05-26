0937bfe6814363e144e53404d4c3b01a658c83cf
Viranch Mehta <viranch.mehta@gmail.com>
2013-05-26 18:17:26 +0530

    way2sms- use xpath to find input elements

--- online/way2sms.py
+++ online/way2sms.py
@@ -1,9 +1,10 @@
 # Doesn't work anymore, needs to be fixed
 import sys
-import time
-import re, random
+import random
 import urllib, urlparse
 import urllib2, cookielib
+from lxml import etree
+from StringIO import StringIO
 
 if len(sys.argv)<5:
     print 'Usage:', sys.argv[0], 'way2sms_username', 'way2sms_password', 'receiver_number', 'text_to_send'
@@ -13,9 +14,7 @@ way2sms = 'site'+str(random.randint(1, 12))+'.way2sms.com'
 
 cj = cookielib.CookieJar()
 opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-opener.addheaders = [('User-Agent','Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.106 Safari/535.2'), ('Host',way2sms), ('Origin','http://'+way2sms)]
-
-grep = re.compile('<input [^>]*>')
+opener.addheaders = [('User-Agent','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36'), ('Host',way2sms), ('Origin','http://'+way2sms)]
 
 def uopen(url, data={}):
     return opener.open(url, urllib.urlencode(data))
@@ -40,29 +39,37 @@ def parseTag(tag, attr):
     return tag[:tag.find(q)]
 
 def send (recvr, text, session_id):
-    data = { 'kriya':'455dasv556', 'catnamedis':'', 'diffNo':'', 't_15_k_5':'', 'm_15_b':'' }
+    s, data = post_data(recvr, text, session_id)
+    data['chkall'] = 'on'
+    s = s[s.find('"name", "')+len('"name", "'):]
+    data[s[:s.find('"')]] = ''
+    print data
+    print urllib.urlencode(data)
+    opener.addheaders.append(('Referer','http://'+way2sms+'/jsp/SingleSMS.jsp?Token='+session_id))
+    f = uopen('http://'+way2sms+'/jsp/w2ssms.action', data)
+    s = f.read()
+    f.close()
+    print f.url
+
+def post_data(recvr, text, session_id):
+    data = {}
     params_url = 'http://'+way2sms+'/jsp/SingleSMS.jsp?Token='+session_id
     f = uopen(params_url)
     s = f.read()
     f.close()
     open('log.html','w').write(s)
-    for i in grep.findall(s):
-        name = parseTag(i, 'name')
-        if name in data.keys(): data[name] = parseTag(i, 'value')
-    data['i_m'] = 'sn2sms'
+    root = etree.parse(StringIO(s), etree.HTMLParser()).getroot()
+    for i in root.xpath('//html/body/form//input'):
+        if i.get('type') not in ['hidden', 'text']: continue
+        data[i.get('name')] = i.get('value')
+    for i in root.xpath('//html/body/form/textarea'):
+        data[i.get('name')] = i.text
     data[data['m_15_b']] = recvr
     data[data['t_15_k_5']] = session_id
     data['textArea'] = text
     data['txtLen'] = 140-len(text)
-    data['chkall'] = 'on'
-    s = s[s.find('"name", "')+len('"name", "'):]
-    data[s[:s.find('"')]] = ''
-    print data
-    opener.addheaders.append(('Referer',params_url))
-    f = uopen('http://'+way2sms+'/jsp/w2ssms.action', data)
-    s = f.read()
-    f.close()
-    print f.url
+    data.pop('textfield2')
+    return s, data
 
 sender = sys.argv[1]
 password = sys.argv[2]
