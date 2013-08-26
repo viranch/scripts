0362d08b100ea0eeaac672c30c0e012db300253d
Viranch Mehta <viranch.mehta@gmail.com>
2013-08-26 16:31:55 +0530

    Airtel usage- handle exceptions, maintain state

--- online/airtel.py
+++ online/airtel.py
@@ -1,4 +1,4 @@
-import sys
+import sys, os
 import urllib, urllib2
 import cookielib
 from lxml import etree
@@ -21,27 +21,45 @@ def login(user, passwd):
         'password': passwd,
         'login-form-type': 'pwd',
     }
-    s = uopen('https://airtel.in/pkmslogin.form', data)
+    try:
+        s = uopen('https://airtel.in/pkmslogin.form', data)
+    except urllib2.URLError:
+        login(user, passwd)
 
 def quota(phone_no, acc_no):
-    s = uopen('https://www.airtel.in/myaccount/NavigationApp/jsp/unbill.jsp?no='+phone_no+'&accno='+acc_no+'&serviceType=DSL')
+    try:
+        s = uopen('https://www.airtel.in/myaccount/NavigationApp/jsp/unbill.jsp?no='+phone_no+'&accno='+acc_no+'&serviceType=DSL')
+    except urllib2.URLError:
+        return quota(phone_no, acc_no)
+
     s = s[s.find('url :"')+6:]
     ajax_url = s[:s.find('"')]
     s = s[s.find('data: "')+7:]
     ajax_data = s[:s.find('"')]
 
-    s = opener.open('https://www.airtel.in'+ajax_url, ajax_data).read()
-    root = etree.parse(StringIO(s), etree.XMLParser()).getroot()
-    used = float(root.xpath('//unbilledDSLUsage/unbilledUsage')[0].text)
-    left = float(root.xpath('//unbilledDSLUsage/gbLeft')[0].text)
-    return used, left
-
-done = False
-while not done:
     try:
-        login(user, passwd)
-        used, left = quota(phone, acc)
-        print str(used)+'/'+str(int(used+left)), 'GB used'
-        done = True
-    except:
-        pass
+        s = opener.open('https://www.airtel.in'+ajax_url, ajax_data).read()
+    except urllib2.URLError:
+        return quota(phone_no, acc_no)
+
+    try:
+        root = etree.parse(StringIO(s), etree.XMLParser()).getroot()
+        used = float(root.xpath('//unbilledDSLUsage/unbilledUsage')[0].text)
+        left = float(root.xpath('//unbilledDSLUsage/gbLeft')[0].text)
+        return used, left
+    except IndexError:
+        return quota(phone_no, acc_no)
+
+def read_usage():
+    try:
+        return float(open(STATE_FILE).read())
+    except IOError:
+        return 0
+
+def save_usage(used):
+    open(STATE_FILE, 'w').write(str(used))
+
+used, left = quota(phone, acc)
+last_used = read_usage()
+print str(used-last_used), 'GB used:', str(used)+'/'+str(int(used+left)), 'GB quota'
+save_usage(used)
