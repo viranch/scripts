2b7b2945001bf099c6621fed03791dee748df9d1
Viranch Mehta <viranch.mehta@gmail.com>
2013-09-27 20:28:08 +0530

    Transmission email script- add support for other recipients

--- online/tr_email.sh
+++ online/tr_email.sh
@@ -5,4 +5,4 @@ source ~/.gmail
 
 url="http://$host/stuff/$TR_TORRENT_NAME"
 
-echo -e "Torrent download $TR_TORRENT_NAME is complete:\n$url" | mailx -S smtp-use-starttls -S ssl-verify=ignore -S smtp-auth=login -S smtp=smtp.gmail.com:587 -S from=pi@alarmpi.me -S smtp-auth-user=$user -S smtp-auth-password=$pass -s "$TR_TORRENT_NAME downloaded" $user@gmail.com
+echo -e "Torrent download $TR_TORRENT_NAME is complete:\n$url" | mailx -S smtp-use-starttls -S ssl-verify=ignore -S smtp-auth=login -S smtp=smtp.gmail.com:587 -S from=pi@alarmpi.me -S smtp-auth-user=$user -S smtp-auth-password=$pass -s "$TR_TORRENT_NAME downloaded" $user@gmail.com $others
