fac24ac2ea2acab6997a0d6410bf1ffa18ca66a2
Viranch Mehta <viranch.mehta@gmail.com>
2013-10-30 05:07:26 +0530

    Revert "tr_email- include optional URL in email"

--- online/tr_email.sh
+++ online/tr_email.sh
@@ -3,6 +3,4 @@
 source ~/.gmail
 (test -z "$user" || test -z "$pass") && exit 1
 
-url="http://$host/stuff/$TR_TORRENT_NAME"
-
-echo -e "Torrent download $TR_TORRENT_NAME is complete:\n$url" | mailx -S smtp-use-starttls -S ssl-verify=ignore -S smtp-auth=login -S smtp=smtp.gmail.com:587 -S from=pi@alarmpi.me -S smtp-auth-user=$user -S smtp-auth-password=$pass -s "$TR_TORRENT_NAME downloaded" $others
+echo "Torrent download $TR_TORRENT_NAME is complete" | mailx -S smtp-use-starttls -S ssl-verify=ignore -S smtp-auth=login -S smtp=smtp.gmail.com:587 -S from=pi@alarmpi.me -S smtp-auth-user=$user -S smtp-auth-password=$pass -s "$TR_TORRENT_NAME downloaded" $others
