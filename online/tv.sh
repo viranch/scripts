36da9c74c52cf9bd6269a668140e4dd085a86594
Viranch Mehta <viranch.mehta@gmail.com>
2013-12-16 20:48:24 +0530

    tv- download only eztv's stuff

--- online/tv.sh
+++ online/tv.sh
@@ -70,5 +70,5 @@ do
         suff=$(echo $line | cut -d':' -f2)
         add_torrent "$title" "$suff"
     done
-    test -n "$all" && add_torrent "$title"
+    test -n "$all" && add_torrent "$title eztv"
 done
