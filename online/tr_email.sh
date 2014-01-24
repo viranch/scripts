c940df22457e28600016f6e9390548e58a6d0f74
Viranch Mehta <viranch.mehta@gmail.com>
2014-01-25 05:16:25 +0530

    tr_email- use newly implemented push notification mechanism

--- online/tr_email.sh
+++ online/tr_email.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-source ~/.gmail
-(test -z "$user" || test -z "$pass") && exit 1
-
-echo "Torrent download $TR_TORRENT_NAME is complete" | mailx -S smtp-use-starttls -S ssl-verify=ignore -S smtp-auth=login -S smtp=smtp.gmail.com:587 -S from=pi@alarmpi.me -S smtp-auth-user=$user -S smtp-auth-password=$pass -s "$TR_TORRENT_NAME downloaded" $others
+source ~/.push
+push transmission@alarmpi.me "$TR_TORRENT_NAME downloaded"
