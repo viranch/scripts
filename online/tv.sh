24573002a2cfd995d36e33d4646af00c37e09bad
Viranch Mehta <viranch.mehta@gmail.com>
2014-02-28 11:30:37 +0530

    tv- fix syntax

--- online/tv.sh
+++ online/tv.sh
@@ -49,7 +49,7 @@ function search() {
     #curl -s http://torrentz.in/feed?q="$@" | grep "<link>.*$" -o | head -n2 | grep -v "search?q=" | sed 's/<link>http:\/\/torrentz\.in\///g' | sed 's/<\/link>//g'
     cookie_file="/tmp/surecook"
     rm -f $cookie_file
-    while [[ ! -f $cookie_file ]]; do; curl -s -XHEAD https://www.suresome.com/ -c $cookie_file > /dev/null; done
+    while [[ ! -f $cookie_file ]]; do curl -s -XHEAD https://www.suresome.com/ -c $cookie_file > /dev/null; done
     curl -s --compressed https://www.suresome.com/proxy/nph-secure/00A/http/torrentz.in/feed%3fq%3d"$@" -b $cookie_file | grep "<link>.*$" -o | head -n2 | grep -v "search?q=" | sed 's/<link>http:\/\/torrentz\.in\///g' | sed 's/<\/link>//g'
 }
 
