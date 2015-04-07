a57148dae0aa65cf8d2c09942caef5cb88fc8bd5
Viranch Mehta <viranch.mehta@gmail.com>
2015-04-07 18:08:39 +0530

    port forward- supply list of ports to forward as CLI arguments

--- online/forward.sh
+++ online/forward.sh
@@ -1,15 +1,21 @@
 # convenience script to NAT traffic on HTTP port to other destination
 
 to=`dig +short $1 | head -n1`
+shift
+
 sudo sysctl net.ipv4.ip_forward=1
-sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination $to
+for i in $@; do
+    sudo iptables -t nat -A PREROUTING -p tcp --dport $i -j DNAT --to-destination $to
+done
 sudo iptables -t nat -A POSTROUTING -j MASQUERADE
 
 echo "Proxying $1, press any key to stop. "
 read _
 
 sudo sysctl net.ipv4.ip_forward=0
-sudo iptables -t nat -D PREROUTING -p tcp --dport 80 -j DNAT --to-destination $to
+for i in $@; do
+    sudo iptables -t nat -D PREROUTING -p tcp --dport $i -j DNAT --to-destination $to
+done
 sudo iptables -t nat -D POSTROUTING -j MASQUERADE
 
 echo "Proxy stopped."
