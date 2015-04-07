b6184512140c2780500ee8e2b3997037d745aa3f
Viranch Mehta <viranch.mehta@gmail.com>
2015-04-07 18:17:26 +0530

    port forward- enable ip_forward only if not already enabled, disable only if it was explicitly enabled

--- online/forward.sh
+++ online/forward.sh
@@ -3,7 +3,13 @@
 dest=$1
 shift
 
-sudo sysctl net.ipv4.ip_forward=1
+ip_needs_fwd=""
+
+if [[ `sysctl -n net.ipv4.ip_forward` == "0" ]]; then
+    ip_needs_fwd="yes"
+    sudo sysctl net.ipv4.ip_forward=1
+fi
+
 to=`dig +short $dest | head -n1`
 for i in $@; do
     sudo iptables -t nat -A PREROUTING -p tcp --dport $i -j DNAT --to-destination $to
@@ -13,10 +19,13 @@ sudo iptables -t nat -A POSTROUTING -j MASQUERADE
 echo "Proxying $dest ($to), press any key to stop."
 read _
 
-sudo sysctl net.ipv4.ip_forward=0
 for i in $@; do
     sudo iptables -t nat -D PREROUTING -p tcp --dport $i -j DNAT --to-destination $to
 done
 sudo iptables -t nat -D POSTROUTING -j MASQUERADE
+
+if [[ -n "ip_needs_fwd" ]]; then
+    sudo sysctl net.ipv4.ip_forward=0
+fi
 
 echo "Proxy stopped."
