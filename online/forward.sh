acecc39ea38a797add966e9730324da8bed328e0
Viranch Mehta <viranch.mehta@gmail.com>
2015-04-07 18:15:47 +0530

    port forward- prettier output

--- online/forward.sh
+++ online/forward.sh
@@ -1,15 +1,16 @@
 # convenience script to NAT traffic on HTTP port to other destination
 
-to=`dig +short $1 | head -n1`
+dest=$1
 shift
 
 sudo sysctl net.ipv4.ip_forward=1
+to=`dig +short $dest | head -n1`
 for i in $@; do
     sudo iptables -t nat -A PREROUTING -p tcp --dport $i -j DNAT --to-destination $to
 done
 sudo iptables -t nat -A POSTROUTING -j MASQUERADE
 
-echo "Proxying $1, press any key to stop. "
+echo "Proxying $dest ($to), press any key to stop."
 read _
 
 sudo sysctl net.ipv4.ip_forward=0
